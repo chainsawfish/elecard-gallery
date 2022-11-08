4cc8e88bee7ae34b493f631ca5df568890ac4418
chainsawfish <chainsawfish@gmail.com>
2022-11-08 22:32:37 +0700

    feat: add modal window to open images

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useState } from "react";
 import style from "./Image.module.css";
 import constants from "../module/constants";
 import getDate from "../module/getDate";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "fit-content",
+  bgcolor: "",
+  border: "0px solid #000",
+};
 
 const Image = ({ imageUrl, fileSize, category, timeStamp, deleteHandler }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   let cleanName = imageUrl
     .split("/")
     .pop()
@@ -16,6 +33,7 @@ const Image = ({ imageUrl, fileSize, category, timeStamp, deleteHandler }) => {
         className={style.image}
         src={constants.IMG_URL + imageUrl}
         alt={imageUrl}
+        onClick={handleOpen}
       />
       <div className={style.imageText}>
         <strong>{category}:</strong>
@@ -26,6 +44,11 @@ const Image = ({ imageUrl, fileSize, category, timeStamp, deleteHandler }) => {
       <div className={style.imageCross} onClick={() => deleteHandler(imageUrl)}>
         ✕
       </div>
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={modalStyle}>
+          <img src={constants.IMG_URL + imageUrl} alt={imageUrl} />
+        </Box>
+      </Modal>
     </div>
   );
 };
