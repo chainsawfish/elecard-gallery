163cb382f738f64679c6eb9e20dc83d34cd59680
chainsawfish <chainsawfish@gmail.com>
2022-11-24 18:14:38 +0700

    fix 2-digit month format for date

--- src/module/getDate.jsx
+++ src/module/getDate.jsx
@@ -2,7 +2,7 @@
 const getDate = (timeStamp) => {
   let d = new Date(timeStamp);
   let ye = new Intl.DateTimeFormat("ru", { year: "numeric" }).format(d);
-  let mo = new Intl.DateTimeFormat("ru", { month: "numeric" }).format(d);
+  let mo = new Intl.DateTimeFormat("ru", { month: "2-digit" }).format(d);
   let da = new Intl.DateTimeFormat("ru", { day: "2-digit" }).format(d);
   return `${da}.${mo}.${ye}`;
 };
