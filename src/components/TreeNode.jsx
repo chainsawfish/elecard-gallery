e65192df8ad4f18c7c6033f2b5063b2d05df1502
chainsawfish <chainsawfish@gmail.com>
2022-12-01 20:01:37 +0700

    fix In TreeView mode now Root not close after click and close modal image window

--- src/components/TreeNode.jsx
+++ src/components/TreeNode.jsx
@@ -11,36 +11,45 @@ const TreeNode = ({image}) => {
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const cachedImage = useMemo(()=>{return image},[])
+    const cachedImage = useMemo(() => {
+        return image
+    }, [])
     let imageUrl = cachedImage.image
 
     return (
         <>
             <h4 className={style.imageNodeName} onClick={(e) => {
-                setSign(!sign)
                 e.stopPropagation()
+                setSign(!sign)
             }}>{constants.isOpened(sign)} {getCleanName(imageUrl)}:</h4>
-        <div className="treeImages" style={constants.changeDisplayStyle(sign)}>
-            <div className={style.imageContainer}  >
-                <img alt={imageUrl}
-                     src={constants.IMG_URL + imageUrl}
-                     onClick={handleOpen}
-                />
-                <div className={style.imageText}>
-                    <p><b>category:</b> {cachedImage.category}</p>
-                    <p><b>name:</b> {getCleanName(imageUrl)}</p>
-                    <p><b>size:</b> {Number(Math.ceil(cachedImage.filesize / 1024))} KB</p>
-                    <p><b>date:</b> {getDate(cachedImage.timestamp)}</p>
+            <div className="treeImages" style={constants.changeDisplayStyle(sign)}>
+                <div className={style.imageContainer}>
+                    <img alt={imageUrl}
+                         src={constants.IMG_URL + imageUrl}
+                         onClick={(e) => {
+                             e.stopPropagation()
+                             handleOpen()
+                         }}
+                    />
+                    <div className={style.imageText}>
+                        <p><b>category:</b> {cachedImage.category}</p>
+                        <p><b>name:</b> {getCleanName(imageUrl)}</p>
+                        <p><b>size:</b> {Number(Math.ceil(cachedImage.filesize / 1024))} KB</p>
+                        <p><b>date:</b> {getDate(cachedImage.timestamp)}</p>
+                    </div>
+
+                    <Modal open={open} onClose={(e => {
+                        e.stopPropagation()
+                        handleClose()
+                    })
+                    }>
+                        <Box sx={constants.modalStyle}>
+                            <img src={constants.IMG_URL + imageUrl} alt={imageUrl}/>
+                        </Box>
+                    </Modal>
                 </div>
 
-                <Modal open={open} onClose={handleClose}>
-                    <Box sx={constants.modalStyle}>
-                        <img src={constants.IMG_URL + imageUrl} alt={imageUrl}/>
-                    </Box>
-                </Modal>
             </div>
-
-        </div>
         </>
     );
 };
