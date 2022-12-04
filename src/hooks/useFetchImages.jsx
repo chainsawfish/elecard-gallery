772130ce035e7820aa7e16a0a1dc9dcaf6fadd56
Denis <chainsawfish@gmail.com>
2022-12-04 22:41:12 +0700

    feat added abortController to fetch hook

--- src/hooks/useFetchImages.jsx
+++ src/hooks/useFetchImages.jsx
@@ -12,22 +12,24 @@ const useFetchImages = (url) => {
     const calculateTotalPages = (arrayOfImages) => Math.ceil(arrayOfImages.length / constants.numberOfImagesOnPage);
     let savedArray = []
     const fetchData = async () => {
+        const controller = new AbortController()
         try {
-            const response = await axios.get(url);
+            const response = await axios.get(url, {signal: controller.signal});
             savedArray = [...response.data].filter((el) => !localStorage.getItem(el.image))
             setImages(savedArray)
             if (allImagesArray.length === 0) setAllImagesArray(response.data)
         } catch (error) {
             console.log(error)
-        }
-        finally {
-            setTotalPages(calculateTotalPages([...savedArray]) )
+        } finally {
+            setTotalPages(calculateTotalPages([...savedArray]))
+            controller.abort()
         }
     }
 
-    useEffect(()=> {
-        fetchData().then(() => {} )
-    },[url] )
+    useEffect(() => {
+        fetchData().then(() => {
+        })
+    }, [url])
     return {images, setImages, totalPages, setTotalPages, allImagesArray}
 }
 export default useFetchImages;
